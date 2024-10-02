73d51baceba0b6d708984128e94c319bfc5b58b9
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-10-02 16:45:43 +0300

    Added get_authenticated_user function that checks if the user is authorized

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import database
+from sqlalchemy.orm import Session
+from fastapi import Request, Depends, HTTPException, status
+from fastapi.responses import RedirectResponse
 
 
 def get_db():
@@ -7,3 +10,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+async def get_authenticated_user(request: Request, db: Session = Depends(get_db)):
+    from auth import get_current_user
+    token = request.cookies.get("access_token")
+
+    if not token:
+        return RedirectResponse(url="/login/", status_code=status.HTTP_303_SEE_OTHER)
+
+    token = token.split(" ")[1] if " " in token else token
+    try:
+        user = get_current_user(token=token, db=db)
+    except HTTPException:
+        return RedirectResponse(url="/login/", status_code=status.HTTP_303_SEE_OTHER)
+
+    return user
