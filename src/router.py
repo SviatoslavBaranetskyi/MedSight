4961ecc1bb7baab86ee65f3550c676acf833eea5
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-10-07 11:37:03 +0300

    Added router for analysis

--- src/router.py
+++ src/router.py
@@ -3,3 +3,4 @@ from fastapi import APIRouter
 
 router_auth = APIRouter(prefix='/auth', tags=['Authorization management'])
 
+router_analysis = APIRouter(prefix='/analysis', tags=['Analysis management'])
